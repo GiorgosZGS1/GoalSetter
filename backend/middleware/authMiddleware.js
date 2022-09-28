8f964c54d51d6a02f6af9d5ff2602b8eb9f92823
Giorgos Zagaris <giorgosz21@hotmail.com>
2022-09-28 11:46:23 +0300

    3rd commit

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -5,7 +5,13 @@ const User = require('../models/userModel');
 const protect = asyncHandler(async(req, res, next) => {
     let token 
 
-    if (req.headers.authorization)
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        try {
+            
+        } catch(error) {
+            
+        }
+    }
 })
 
 module.exports = {
